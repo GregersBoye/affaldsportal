25d36344892242486db154b4f5d0b9ddc76e6b72
Gregers Boye-Jacobsen <gbj@mydefence.dk>
2019-04-23 21:29:46 +0200

    changed api to use port 5000 instead of 3000

--- api/src/index.js
+++ api/src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const ical = require('ical-generator');
-const port = 3000;
+const port = 5000;
 const moment = require('moment');
 const Portal = require('./interfaces/AffaldsPortal');
 
