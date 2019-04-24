78bba1250eb222b04451030a916ad13375b411a8
Gregers Boye-Jacobsen <gbj@cego.dk>
2019-04-24 17:37:31 +0200

    switch to using bulma in the frontend and setup form

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -1,19 +1,34 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
+import "bulma";
 
 class Main extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            address: 'my address'
+        }
+    }
+
+    handleChange = async (event) => {
+        this.setState({address: event.target.value});
+        const result = await fetch("http://localhost:4000/address/getServices?addId=17034")
+        console.log(result);
+
+    };
     render() {
         return (
-            <div>
-                <h1>Simple SPA</h1>
-                <ul className="header">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/stuff">Stuff</a></li>
-                    <li><a href="/contact">Contact</a></li>
-                </ul>
-                <div className="content">
-
+            <section className="section">
+                <div className="container">
+                    <h1 className="title">
+                        Hello World
+                    </h1>
+                    <p className="subtitle">
+                        My first website with <strong>Bulma</strong> and <strong>React</strong>!
+                    </p>
+                    <input type="text" value={this.state.address} onChange={this.handleChange}/>
                 </div>
-            </div>
+            </section>
         );
     }
 }
